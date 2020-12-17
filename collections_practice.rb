81491448ce111b171225985cb3ea029baa3b732d
Ramon <ramm050489@gmail.com>
2020-12-17 15:41:08 -0500

    commit tree 15daaa6c99c5133b9ad502573cf6c9ac26e62b5d

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+    array.sort do |a, b|
+        a <=> b 
+    end
+end
 
+def sort_array_desc(array)
+    array.sort do |a, b| 
+         b <=> a 
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |left, right|
+        left.length <=> right.length
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array [1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |item|
+        item[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select {|i| i.start_with?("a")}
+end
+
+def sum_array(array)
+    array.inject do |sum, n|
+        sum + n
+    end
+end
+
+def add_s(array)
+    array.collect do |word|
+        if array[1] == word
+            word
+        else
+            word + "s"
+        end
+    end
+end
+
+        
